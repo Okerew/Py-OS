aa2f2cbf444a6833620d7d8be3f1b4fa13dbeae8
Okerew <93822247+Okerew@users.noreply.github.com>
2023-11-16 17:26:54 +0100

    Update core_commands.py

--- core_commands.py
+++ core_commands.py
@@ -1,18 +1,21 @@
 # core_commands.py
-import shutil
-import os
-from run_apps import commands
-import platform
-import code_runner
-import sys
 import hashlib
+import os
+import platform
+import shutil
 import subprocess
+import sys
+import git
+import code_runner
+from run_apps import commands
+import datetime
 
 class CoreCommands:
     def __init__(self):
         self.current_folder = os.getcwd()
         self.logged_in_user = None
         self.user_data_file = "users.pexof"
+        self.aliases = {}
         self.commands = {
             **commands,
             "help": "Display available commands",
@@ -27,6 +30,12 @@ class CoreCommands:
             "code_runner": "Runs python, java,c scripts",
             "restart": "Restarts the system",
             "clear": "clears the terminal",
+            "about": "Tells information about an app",
+            "alias": "Manage aliases (shortcuts)",
+            "change_password": "allows you to change your password",
+            "uptade": "uptades the system",
+            "backup": "backups the system",
+            "time": "shows the time(year,month,day,hour, minute, second"
         }
 
     def run_command(self, command):
@@ -61,8 +70,18 @@ class CoreCommands:
                 self.restart_program()
             elif command == "clear":
                 self.clear_screen()
-            else:
-                self.run_app(command)
+            elif command == "about":
+                self.show_about_info()
+            elif command == "alias":
+                self.alias_command()
+            elif command == "change_password":
+                self.change_password()
+            elif command == "time":
+                self.show_datetime()
+            elif command == "update": 
+                self.update_system()
+            elif command == "backup":
+                self.backup_system()
         else:
             print("Unknown command.")
 
@@ -90,21 +109,32 @@ class CoreCommands:
             print(f"An error occurred while running '{app_name}': {e}")
 
     def clean_pyc_files(self, folder="."):
-        pyc_files = []
+      pyc_files = []
 
-        for root, dirs, files in os.walk(folder):
-            pyc_files.extend([os.path.join(root, f) for f in files if f.endswith(".pyc")])
+      for root, dirs, files in os.walk(folder):
+          pyc_files.extend([os.path.join(root, f) for f in files if f.endswith(".pyc")])
 
-        if pyc_files:
-            print("Cleaning up .pyc files...")
-            for pyc_file in pyc_files:
-                if os.path.exists(pyc_file):
-                    print(f"Removing {pyc_file}...")
-                    os.remove(pyc_file)
-                    print(f"Removed {pyc_file}")
-            print("Cleaned up .pyc files.")
-        else:
-            print("No .pyc files found.")
+      if pyc_files:
+          print("Cleaning up .pyc files...")
+          for pyc_file in pyc_files:
+              if os.path.exists(pyc_file):
+                  print(f"Removing {pyc_file}...")
+                  os.remove(pyc_file)
+                  print(f"Removed {pyc_file}")
+          print("Cleaned up .pyc files.")
+      else:
+          print("No .pyc files found.")
+      # Remove backup folders
+      backup_folders = [f for f in os.listdir(folder) if os.path.isdir(os.path.join(folder, f)) and f.startswith("backup")]
+      if backup_folders:
+          print("Cleaning up backup folders...")
+          for backup_folder in backup_folders:
+              backup_folder_path = os.path.join(folder, backup_folder)
+              shutil.rmtree(backup_folder_path)
+              print(f"Removed backup folder: {backup_folder}")
+          print("Cleaned up backup folders.")
+      else:
+          print("No backup folders found.")
 
     def git_clone(self, repo_url):
         if not self.logged_in_user:
@@ -174,6 +204,16 @@ class CoreCommands:
         else:
             os.environ["TERM"] = "xterm"
             os.system("clear")
+    def show_about_info(self):
+        app_name = input("Enter the app name: ")
+        about_file_path = os.path.join("apps", "about", f"{app_name}.txt")
+
+        if os.path.exists(about_file_path):
+            with open(about_file_path, "r") as about_file:
+                about_content = about_file.read()
+                print(f"About {app_name}:\n{about_content}")
+        else:
+            print(f"About information for {app_name} not found.")
 
     def create_user(self, username, password):
         #creating the user and encoding the password
@@ -207,3 +247,95 @@ class CoreCommands:
                         return
 
         print("Login failed. Please check your username and password.")
+
+    def alias_command(self):
+      subcommand = input("Enter 'set', 'remove', or 'list': ")
+
+      if subcommand == "set":
+          alias_name = input("Enter the alias name: ")
+          alias_value = input("Enter the alias value: ")
+          self.aliases[alias_name] = alias_value
+          print(f"Alias '{alias_name}' set to '{alias_value}'.")
+      elif subcommand == "remove":
+          alias_name = input("Enter the alias name to remove: ")
+          if alias_name in self.aliases:
+              del self.aliases[alias_name]
+              print(f"Alias '{alias_name}' removed.")
+          else:
+              print(f"Alias '{alias_name}' not found.")
+      elif subcommand == "list":
+          print("Current aliases:")
+          for alias_name, alias_value in self.aliases.items():
+              print(f"{alias_name} - {alias_value}")
+      else:
+          print("Invalid subcommand. Please use 'set', 'remove', or 'list'.")
+
+    def change_password(self):
+      if not self.logged_in_user:
+          print("You must be logged in to change your password.")
+          return
+
+      current_password = input("Enter your current password: ")
+
+      # Validate the current password
+      if self.validate_password(current_password):
+          new_password = input("Enter your new password: ")
+
+          # Implement logic to securely change the user's password (placeholder)
+          hashed_new_password = hashlib.sha256(new_password.encode()).hexdigest()
+          self.update_password_in_file(hashed_new_password)
+
+          print("Password changed successfully.")
+      else:
+          print("Incorrect current password. Password change failed.")
+
+    def validate_password(self, input_password):
+      # Validate the current password against stored hashed password
+      hashed_input_password = hashlib.sha256(input_password.encode()).hexdigest()
+
+      with open(self.user_data_file, "r") as file:
+          user_data = file.read()
+          return f"Password: {hashed_input_password}" in user_data
+
+    def update_password_in_file(self, new_hashed_password):
+      # Update the stored hashed password in the user data file
+      with open(self.user_data_file, "r") as file:
+          user_data = file.read()
+
+      # Find and replace the old hashed password with the new one
+      old_hashed_password = user_data.split("Password: ")[1].split("\n")[0]
+      user_data = user_data.replace(old_hashed_password, new_hashed_password)
+
+      with open(self.user_data_file, "w") as file:
+          file.write(user_data)
+    
+    def update_system(self):
+      try:
+          repo_url = "https://github.com/Okerew/Py-OS.git"
+          repo_folder = "Py-OS" 
+          git.Repo.clone_from(repo_url, repo_folder)
+          self.backup_system()
+          print("Creating a backup of the system.")
+          for root, _dirs, files in os.walk(repo_folder):
+              for file in files:
+                  src_path = os.path.join(root, file)
+                  dest_path = os.path.join(self.current_folder, file)
+                  shutil.copy2(src_path, dest_path)
+
+          print("System updated successfully. Restart the system to apply changes.")
+      except Exception as e:
+          print(f"Error updating system: {e}")
+    
+    def backup_system(self):
+      timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+      backup_folder = f"backup_{timestamp}"
+      try:
+          ignore_patterns = shutil.ignore_patterns(backup_folder)
+          shutil.copytree(self.current_folder, backup_folder, ignore=ignore_patterns)
+          print(f"Backup created successfully: {backup_folder}")
+      except Exception as e:
+          print(f"Error creating backup: {e}")
+    def show_datetime(self):
+      current_datetime = datetime.datetime.now()
+      formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+      print(f"Current Date and Time: {formatted_datetime}")
